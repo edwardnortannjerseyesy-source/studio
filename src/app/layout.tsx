6af682d73f181a14e4658b323d9ee884aea3cbb0
Edward Norton Jersey <edwardnortannjerseyesy@gmail.com>
2026-01-14 03:17:04 +0000

    ALETERE O NOME QUE APARECE NA PARTE DA GUIA, NA PARTE DO HEADER LA ENCIM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import Script from 'next/script';
 
 export const metadata: Metadata = {
-  title: 'Shopee Clone',
+  title: 'Shopee',
   description: 'A clone of a Shopee product page.',
 };
 
